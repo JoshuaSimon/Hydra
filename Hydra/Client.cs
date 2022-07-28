315c2e90919325070e241eafdb0153e47af1973a
JoshuaSimon <joshsimon96@googlemail.com>
2022-07-29 00:15:08 +0200

    Added more class structure

--- Hydra/Client.cs
+++ Hydra/Client.cs
@@ -3,8 +3,15 @@ namespace Hydra
 {
     public class Client
     {
-        public Client()
+        public int id;
+        public float capacity;
+        public float load;
+
+        public Client(int id, float capacity, float load = 0.0f)
         {
+            this.id = id;
+            this.capacity = capacity;
+            this.load = load;
         }
     }
 }
--- Hydra/JobManager.cs
+++ Hydra/JobManager.cs
@@ -3,8 +3,13 @@ namespace Hydra
 {
     public class JobManager
     {
+        public List<Job> jobList;
+        public List<Client> clientList;
+
         public JobManager()
         {
+            this.jobList = new List<Job>();
+            this.clientList = new List<Client>();
         }
     }
 }
