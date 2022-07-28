8bad1f943034a0aa7371ad658028c271f06e1398
JoshuaSimon <joshsimon96@googlemail.com>
2022-07-29 00:05:20 +0200

    Added classes for job, client and job manager

---
+++ Hydra/Job.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Hydra
+{
+    public class Job
+    {
+        public int id;
+        public int metaId;
+        public float cost;
+        public string name;
+        public bool isLocked;
+        public int clientId;
+
+        public Job(int id, int metaId, float cost, string name, bool isLocked, int clientId)
+        {
+            this.id = id;
+            this.metaId = metaId;
+            this.cost = cost;
+            this.name = name;
+            this.isLocked = isLocked;
+            this.clientId = clientId;
+        }
+    }
+}
+
---
+++ Hydra/JobManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Hydra
+{
+    public class JobManager
+    {
+        public JobManager()
+        {
+        }
+    }
+}
+
